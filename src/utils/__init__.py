585ca8fdda872c70b5b8aede2dca061ad2db7a2e
enheragu <enrique.he.ag@gmail.com>
2024-03-05 20:14:18 +0100

    Missing init file to commit

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -4,4 +4,4 @@ from .config_utils import parseYaml, dumpYaml, handleArguments, configArgParser
 from .config_utils import generateCFGFiles, clearCFGFIles
 from .config_utils import yolo_output_path, yolo_outpu_log_path, dataset_config_path, dataset_tags_default
 # from .log_utils import isTimetableActive, sleep_until
-from .id_tag import getGPUTestID
+from .id_tag import getGPUTestID, getGPUTestIDTag
