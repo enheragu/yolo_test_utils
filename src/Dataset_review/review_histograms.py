996f410678fb9a8f2f101ff3d0124ef831e4edfe
enheragu <enrique.he.ag@gmail.com>
2024-05-08 10:28:12 +0200

    Reformat review_histograms script with dict and list of histograms insetead of having them separately. Better names and plots with specific info needed

--- src/Dataset_review/review_histograms.py
+++ src/Dataset_review/review_histograms.py
@@ -27,12 +27,6 @@ lwir_folder_name = "lwir"
 
 store_path = f"{home}/eeha/dataset_analysis/"
 
-# Output dataset whitelist (default is original Kaist sets)
-white_list = ['test-all-01','test-day-20','train-all-01','train-all-20','train-day-20','train-night-20',
-              'test-all-20','test-night-01','train-all-02','train-day-02','train-night-02',
-              'test-day-01','test-night-20','train-all-04','train-day-04','train-night-04']
-
-
 
 def save_images(img1, img2, hist1, hist2, filename, img_path):
     plt.figure()
@@ -77,68 +71,61 @@ def gethistEq(img):
     return eq_img, hist
 
 
-def extract_hist(img_path, img_type, plot = False):
+def extract_hist(img_path, plot = False):
     b, g, r, lwir = [], [], [], []
     eq_b, eq_g, eq_r, eq_lwir = [], [], [], []
 
-    if img_type == 'rgb':
-        img = cv.imread(img_path)
-        assert img is not None, "file could not be read, check with os.path.exists()"
-        b = [cv.calcHist([img], [0], None, [256], [0, 256])]
-        g = [cv.calcHist([img], [1], None, [256], [0, 256])]
-        r = [cv.calcHist([img], [2], None, [256], [0, 256])]
-        
-        b_ch,g_ch,r_ch = cv.split(img)
-        eq_b = [gethistEqCLAHE(b_ch)[1]]
-        eq_g = [gethistEqCLAHE(g_ch)[1]]
-        eq_r = [gethistEqCLAHE(r_ch)[1]]
+    # Process visible image
+    img = cv.imread(img_path)
+    assert img is not None, "file could not be read, check with os.path.exists()"
+    b = cv.calcHist([img], [0], None, [256], [0, 256])
+    g = cv.calcHist([img], [1], None, [256], [0, 256])
+    r = cv.calcHist([img], [2], None, [256], [0, 256])
+    
+    b_ch,g_ch,r_ch = cv.split(img)
+    eq_b = gethistEqCLAHE(b_ch)[1]
+    eq_g = gethistEqCLAHE(g_ch)[1]
+    eq_r = gethistEqCLAHE(r_ch)[1]
 
-    elif img_type == 'lwir':
-        img = cv.imread(img_path, cv.IMREAD_GRAYSCALE)
-        assert img is not None, "file could not be read, check with os.path.exists()"
+    # Process LWIR correspondant image
+    img_path = img_path.replace(visible_folder_name, lwir_folder_name)
+    img = cv.imread(img_path, cv.IMREAD_GRAYSCALE)
+    assert img is not None, "file could not be read, check with os.path.exists()"
+    
+    lwir = cv.calcHist([img], [0], None, [256], [0, 256])
+    eq_lwir = gethistEqCLAHE(img)[1]
         
-        lwir = [cv.calcHist([img], [0], None, [256], [0, 256])]
-        eq_lwir = [gethistEqCLAHE(img)[1]]
-        
-       
-    return (b, g, r, lwir), (eq_b, eq_g, eq_r, eq_lwir)
+    return [b, g, r, lwir], [eq_b, eq_g, eq_r, eq_lwir]
 
 
-def process_image(path, image_type):
-    b, g, r, lwir = [], [], [], []
-    eq_b, eq_g, eq_r, eq_lwir = [], [], [], []
+def process_image(path):
+    hist = []
+    eq_hist = []
     for file in os.listdir(path):
         if file.endswith((".jpg", ".jpeg", ".pdf", ".npy", ".npz")):
             img_path = os.path.join(path, file)
-            ret_rgb, ret_lwir = extract_hist(img_path, image_type)
+            ret_hist, ret_eq_hist = extract_hist(img_path)
 
-            b.extend(ret_rgb[0])
-            g.extend(ret_rgb[1])
-            r.extend(ret_rgb[2])
-            lwir.extend(ret_rgb[3])
+            hist.append(ret_hist)
+            eq_hist.append(ret_eq_hist)
 
-            eq_b.extend(ret_lwir[0])
-            eq_g.extend(ret_lwir[1])
-            eq_r.extend(ret_lwir[2])
-            eq_lwir.extend(ret_lwir[3])
-
-    return (b, g, r, lwir), (eq_b, eq_g, eq_r, eq_lwir)
+    return hist, eq_hist, path
 
 
 
-def save_histogram_image(hist, title, filename, color, n_images, log_scale = False):
+def save_histogram_image(hist, title, filename, color, n_images, log_scale = False, condition = '-'):
     plt.figure(figsize=(4.5, 3))
     # plt.plot(hist, color = color)
     # plt.fill_between(np.arange(len(hist)), hist, color=color, alpha=0.3)
     # plt.hist(hist, bins=range(len(hist)), color=color, alpha=0.7)
 
-    plt.plot(hist[1], color = color, label = f"{title} channel histogram (max)")
+    plt.plot(hist[1], color = color, label = f"{title} (max)")
     # plt.plot(hist[0], color = color)
     # plt.fill_between(np.arange(len(hist[0])), hist[0], color=color, alpha=0.8, label = "min") # between 0 and min
     plt.fill_between(np.arange(len(hist[0])), hist[1], color=color, alpha=0.3) #, label = "variance") # between 0 and max
     if log_scale: plt.yscale('log')
 
-    plt.title(f'{title} histogram ({n_images} images)')
+    plt.title(f'{title} histogram ({n_images} images) ({condition} condition)')
     plt.xlabel('Pixel Value')
     plt.ylabel('Frequency' + (' (log scale)' if log_scale else ''))
     
@@ -147,35 +134,36 @@ def save_histogram_image(hist, title, filename, color, n_images, log_scale = Fal
     plt.close()
 
 
-def plot_histograms(b_hist, g_hist, r_hist, lwir_hist, titles, colors, filename = 'histograms.pdf', n_images = 0, log_scale = False):
+def plot_histograms(b_hist, g_hist, r_hist, lwir_hist, titles, colors, filename = 'histograms.pdf', n_images = 0, log_scale = False, condition = '-'):
     plt.figure(figsize=(9, 3))
 
     # First subplot
     plt.subplot(1, 2, 1)
-    for index, (hist, title, color) in enumerate(zip([b_hist, g_hist, r_hist], titles, colors)):     
+    for hist, title, color in zip([b_hist, g_hist, r_hist], titles, colors):
         # plt.hist(channel, bins=range(len(channel)), color=color, alpha=0.7)
-        plt.plot(hist[1], color = color, label = f"{title} channel histogram (max)")
+        plt.plot(hist[1], color = color, label = f"{title} (max)")
         # plt.plot(hist[0], color = color)
         # plt.fill_between(np.arange(len(hist[0])), hist[0], color=color, alpha=0.8, label = "min histogram") # between 0 and min
         plt.fill_between(np.arange(len(hist[0])), hist[1], color=color, alpha=0.3) #, label = "variance") # between 0 and max
         if log_scale: plt.yscale('log')
-        save_histogram_image(hist, title, os.path.join(store_path, f'histograms_{title.lower().replace(" ", "_")}{"_log" if log_scale else ""}.pdf'), color=color, n_images=n_images, log_scale=log_scale)
+        # Single histogram for each BGR channel
+        # save_histogram_image(hist, title, os.path.join(store_path, f'{title.lower().replace(" ", "_")}_{filename}'), color=color, n_images=n_images, log_scale=log_scale)
     
     plt.legend()
-    plt.title(f'RGB histograms ({n_images} images)')
+    plt.title(f'RGB histograms ({n_images} images) ({condition})')
     plt.xlabel('Pixel Value')
     plt.ylabel('Frequency' + (' (log scale)' if log_scale else ''))
 
     # Second subplot
     plt.subplot(1, 2, 2)
-    plt.plot(lwir_hist[1], color = colors[-1], label = f"{titles[-1]} channel histogram (max)")
+    plt.plot(lwir_hist[1], color = colors[-1], label = f"{titles[-1]} (max)")
     # plt.plot(lwir_hist[0], color = colors[-1])
     # plt.fill_between(np.arange(len(lwir_hist[0])), lwir_hist[0], color=colors[-1], alpha=0.8, label = "min histogram") # between 0 and min
     plt.fill_between(np.arange(len(lwir_hist[0])), lwir_hist[1], color=colors[-1], alpha=0.3) #, label = "variance") # Minimum in between 0 and max
     if log_scale: plt.yscale('log')
-    save_histogram_image(lwir_hist, titles[-1], os.path.join(store_path, f'histograms_{titles[-1].lower().replace(" ", "_")}{"_log" if log_scale else ""}.pdf'), color=colors[-1], n_images = n_images, log_scale=log_scale)
+    save_histogram_image(lwir_hist, titles[-1], os.path.join(store_path, f'{titles[-1].lower().replace(" ", "_")}_{filename}'), color=colors[-1], n_images = n_images, log_scale=log_scale)
     plt.legend()
-    plt.title(f'LWIR histogram ({n_images} images)')
+    plt.title(f'LWIR histogram ({n_images} images) ({condition})')
     plt.xlabel('Pixel Value')
     plt.ylabel('Frequency' + (' (log scale)' if log_scale else ''))
 
@@ -183,12 +171,31 @@ def plot_histograms(b_hist, g_hist, r_hist, lwir_hist, titles, colors, filename 
     plt.savefig(os.path.join(store_path,filename))
     # plt.show()
 
-set_info = {'day': {'sets': ['set00', 'set01', 'set02', 'set06', 'set07', 'set08'], 'num_img': 0, 'test': [], 'train': []},
-            'night': {'sets': ['set03', 'set04', 'set05', 'set06', 'set10', 'set11'], 'num_img': 0, 'test': [], 'train': []}}
+# Store histograms in a list of [b,g,r,lwir] hists for each image
+set_info = {'day': {'sets': ['set00', 'set01', 'set02', 'set06', 'set07', 'set08'], 'hist': [], 'CLAHE hist': []},
+            'night': {'sets': ['set03', 'set04', 'set05', 'set06', 'set10', 'set11'], 'hist': [], 'CLAHE hist': []},
+            'day+night': {'hist': [], 'CLAHE hist': []}}
 
+
+# Condition is day/night
+def isPathCondition(condition, path):
+    for img_set in set_info[condition]['sets']:
+        if img_set in path:
+            return True
+    return False
+
+# Compute shape of list of lists :)
+
+def shape(lista):
+    if isinstance(lista, list):
+        return [len(lista)] + shape(lista[0]) if lista else []
+    elif isinstance(lista, np.ndarray):
+        return list(lista.shape)
+    else:
+        return []
+
+    
 def evaluateInputDataset():
-    b_hist, g_hist, r_hist, lwir_hist = [], [], [], []
-    eq_b_hist, eq_g_hist, eq_r_hist, eq_lwir_hist = [], [], [], []
 
     with ThreadPoolExecutor() as executor:
         futures = []
@@ -198,50 +205,60 @@ def evaluateInputDataset():
                 if os.path.isdir(path_set):
                     visible_folder = os.path.join(path_set, visible_folder_name)
                     if os.path.exists(visible_folder):
-                        futures.append(executor.submit(process_image, visible_folder, 'rgb'))
-                    
-                    lwir_folder = os.path.join(path_set, lwir_folder_name)
-                    if os.path.exists(lwir_folder):
-                        futures.append(executor.submit(process_image, lwir_folder, 'lwir'))
-                        # process_image(lwir_folder, 'lwir')
+                        futures.append(executor.submit(process_image, visible_folder))
                 # break
             # break
 
-        for future in tqdm(as_completed(futures), total=len(futures), desc='Processing images'):
-            (b, g, r, lwir), (eq_b, eq_g, eq_r, eq_lwir) = future.result()
-            b_hist.extend(b)
-            g_hist.extend(g)
-            r_hist.extend(r)
-            lwir_hist.extend(lwir)
-            eq_b_hist.extend(eq_b)
-            eq_g_hist.extend(eq_g)
-            eq_r_hist.extend(eq_r)
-            eq_lwir_hist.extend(eq_lwir)
+        for future in tqdm(as_completed(futures), total=len(futures), desc='Processing imageSet folders'):
+            hist, eq_hist, path = future.result()
+            for condition in ['day', 'night']:
+                if isPathCondition(condition, path):
+                    set_info[condition]['hist'].extend(hist)
+                    set_info[condition]['CLAHE hist'].extend(eq_hist)
+                    break
     
-    b_data = (np.min(b_hist, axis=0)[:,0], np.max(b_hist, axis=0)[:,0])
-    g_data = (np.min(g_hist, axis=0)[:,0], np.max(g_hist, axis=0)[:,0])
-    r_data = (np.min(r_hist, axis=0)[:,0], np.max(r_hist, axis=0)[:,0])
-    lwir_data = (np.min(lwir_hist, axis=0)[:,0], np.max(lwir_hist, axis=0)[:,0])
 
-
-    eq_b_data = (np.min(eq_b_hist, axis=0)[:,0], np.max(eq_b_hist, axis=0)[:,0])
-    eq_g_data = (np.min(eq_g_hist, axis=0)[:,0], np.max(eq_g_hist, axis=0)[:,0])
-    eq_r_data = (np.min(eq_r_hist, axis=0)[:,0], np.max(eq_r_hist, axis=0)[:,0])
-    eq_lwir_data = (np.min(eq_lwir_hist, axis=0)[:,0], np.max(eq_lwir_hist, axis=0)[:,0])
-
-    for log_scale in [True, False]:
-        plot_histograms(b_data, g_data, r_data, lwir_data,
-                        ["B","G","R","LWIR"], 
-                        ['#0171BA', '#78B01C', '#F23535', '#F6AE2D'],
-                        f'histograms_all{"_log" if log_scale else ""}.pdf',
-                        n_images=len(b_hist), log_scale=log_scale)
     
-        ###### Equalized
-        plot_histograms(eq_b_data, eq_g_data, eq_r_data, eq_lwir_data,
-                        ["CLAHE B","CLAHE G","CLAHE R","CLAHE LWIR"], 
-                        ['#0171BA', '#78B01C', '#F23535', '#F6AE2D'],
-                        f'histograms_eq_all{"_log" if log_scale else ""}.pdf',
-                        n_images=len(eq_b_hist), log_scale=log_scale)
+    def plotAccumulatedHist(condition, data, type):
+        hist_data = []
+        for ch in range(4):
+            min_vals = np.min(data[ch], axis=0)[:,0]
+            max_vals = np.max(data[ch], axis=0)[:,0]
+            hist_data.append([min_vals, max_vals])
+        
+        if isinstance(type, list):
+            b_type, g_type, r_type, lwir_type = type
+        else:
+            b_type, g_type, r_type, lwir_type = type, type, type, type
+
+
+        for log_scale in [True, False]:
+            plot_histograms(hist_data[0], hist_data[1], hist_data[2], hist_data[3],
+                            [f"B {b_type}",f"G {g_type}",f"R {r_type}",f"LWIR {lwir_type}"], 
+                            ['#0171BA', '#78B01C', '#F23535', '#F6AE2D'],
+                            f'{"log_" if log_scale else ""}{condition}.pdf',
+                            n_images=len(data[ch]), log_scale=log_scale, condition = condition)
+
+             
+    for condition in ['day', 'night']:
+        for type in ['hist', 'CLAHE hist']:
+            set_info['day+night'][type].extend(set_info[condition][type])
+            # Theres n_images array of 4 elements, we want 4 arrays of hists. Need to transpose :)
+            set_info[condition][type] = [[channel[i] for channel in set_info[condition][type]] for i in range(4)]
+            # print(f"{condition} {type} shape(data)={shape(set_info[condition][type])}")
+            # plotAccumulatedHist(condition, set_info[condition][type], type)
+    
+    condition = 'day+night'
+    for type in ['hist', 'CLAHE hist']:
+        set_info[condition][type] = [[channel[i] for channel in set_info[condition][type]] for i in range(4)]
+        # print(f"{condition} {type} shape(data)={shape(set_info[condition][type])}")
+        # plotAccumulatedHist(condition, set_info[condition][type], type)
+
+
+    # PLOT BRG hist and LWIR CLAHE
+    for condition in ['day', 'night', 'day+night']:
+        data = [set_info[condition]['hist'][0], set_info[condition]['hist'][1], set_info[condition]['hist'][2], set_info[condition]['CLAHE hist'][3]]
+        plotAccumulatedHist(condition, data, ['hist', 'hist', 'hist', 'CLAHE hist'])
 
 
 if __name__ == '__main__':
