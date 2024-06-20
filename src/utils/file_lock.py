feb120c630103fe644d1ad1013c06ee088ba5db4
enheragu <enrique.he.ag@gmail.com>
2024-06-20 16:12:55 +0200

    Locking has option to make a blocking lock or non-blocking one

--- src/utils/file_lock.py
+++ src/utils/file_lock.py
@@ -1,6 +1,7 @@
 
 import os
 import fcntl
+import time
 
 """
     Class that handles safe lock/unlock mechanism for files. It is set
@@ -9,14 +10,23 @@ import fcntl
 """
 
 class FileLock:
-    def __init__(self, file):
+    def __init__(self, file, blocking = False):
         self.file = file
+        self.blocking = blocking
         if not os.path.exists(self.file): # Create file and lock
             with open(self.file, 'w+'):
                 pass
 
     def __enter__(self):
         fcntl.flock(self.file.fileno(), fcntl.LOCK_EX)
+
+        while self.blocking:
+            try:
+                fcntl.flock(self.file.fileno(), fcntl.LOCK_EX | fcntl.LOCK_NB)
+                break  # Si se obtiene el bloqueo, salir del bucle
+            except BlockingIOError:
+                # Si el bloqueo está en uso, esperar y volver a intentarlo
+                time.sleep(0.1)
 
     def __exit__(self, exc_type, exc_value, traceback):
         fcntl.flock(self.file.fileno(), fcntl.LOCK_UN)
