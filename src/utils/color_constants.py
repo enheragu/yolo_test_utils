ce0c1cf9d8337d2af2cfd4e98ab9b6b190b10924
enheragu <enrique.he.ag@gmail.com>
2024-09-19 12:10:31 +0200

    Add separate file for color constants to be used in plots and logging functions

---
+++ src/utils/color_constants.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+## Custom color definitions
+c_blue = "#0171ba"
+c_green = "#78b01c"
+c_yellow = "#f6ae2d"
+c_red = "#f23535" 
+c_purple = "#a66497"
+c_grey = "#769393"
+c_darkgrey = "#2a2b2e"
+
+color_palette_list = [c_blue,c_green,c_yellow,c_red,c_purple,c_grey,c_darkgrey]
